d850287df5d79127d282b41276e8bb49777e321e
David Earn <earn@math.mcmaster.ca>
2018-02-16 15:15:51 -0500

    fixed bug in Q.R associated with referring to i inside draw.soln

--- Q.R
+++ Q.R
@@ -1,10 +1,11 @@
 library(deSolve)
 ## Vector Field for SIR model
-SIR.vector.field <- function(t, vars=c(S,I,R,Q,X), parms=c(R_0,1/gamma,delta,mu)) {
-  with(as.list(c(parms, vars)), {
-    dS <- -gamma*R_0*S*I # dS/dt
-    dQ <- gamma*R_0*S*I# cumulative cases
-    dX <- gamma*R_0*S*I#cumulative normally infected cases
+SIR.vector.field <- function(t, vars, parms) {
+  ##SIR.vector.field <- function(t, vars=c(S=S,I=I,R=R,Q=Q,X=X), parms=c(R_0=R_0,gamma=gamma,delta=delta,mu=mu)) {
+    with(as.list(c(parms, vars)), {
+    dS <- -gamma*R_0*S*I - delta*S # dS/dt
+    dQ <- gamma*R_0*S*I + delta*S # cumulative cases
+    dX <- gamma*R_0*S*I #cumulative normally infected cases
     dI <- dQ - gamma*I # dI/dt
     dR <- gamma*I #dR/dt
     vec.fld <- c(dS=dS, dI=dI, dR=dR, dQ=dQ, dX=dX)
@@ -12,15 +13,16 @@ SIR.vector.field <- function(t, vars=c(S,I,R,Q,X), parms=c(R_0,1/gamma,delta,mu)
   })
 }
 
-draw.soln <- function(ic=c(S=1,I=0), tmax=1,
+draw.soln <- function(ic=c(S=1,I=0,R=0,Q=0,X=0), tmax=1,
                       times=seq(0,tmax,by=tmax/500),
                       func, parms, ... ) {
   soln <- ode(ic, times, func, parms)
-  lines(times, soln[,"Q"], col=vary_delta[i], lwd=2,... )
+  lines(times, soln[,"Q"], lwd=2,... )
+  return(invisible(as.data.frame(soln)))
 }
 
 ## Plot solutions of the SIR model
-tmax <- 100 # end time for numerical integration of the ODE
+tmax <- 10 # end time for numerical integration of the ODE
 ## draw box for plot:
 plot(0,0,xlim=c(0,tmax),ylim=c(0,1),
      type="n",xlab="Time (t)",ylab="I",las=1)
@@ -31,14 +33,15 @@ R0 <- 1 - I0 - S0
 Q0 <- I0
 X0 <- Q0
 ## draw solutions for several values of parameter R_0:
-vary_delta <- c(1,1.5,2)
+vary_delta <- c(1,0.5,1.5,2)
 
 for (i in 1:length(vary_delta)) {
   draw.soln(ic=c(S=S0,I=I0,R=R0,Q=Q0,X=X0), tmax=tmax,
             func=SIR.vector.field,
-            parms=c(R_0=2,gamma=1/4,delta=vary_delta[i],mu=0.1),
+            parms=c(R_0=2,gamma=1,delta=vary_delta[i],mu=0.1),
+            col=i,
             lty=i # use a different line style for each solution
   )
 }
-legend("topleft",legend=vary_delta,col=vary_delta,lty=1:3)
+legend("bottomright",legend=vary_delta,col=1:length(vary_delta),lty=1:length(vary_delta))
 
