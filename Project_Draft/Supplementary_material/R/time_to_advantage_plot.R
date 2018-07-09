0c9bdc346ac31183cd9bdccc7c5559420838d63d
rgrzhang <35642578+rgrzhang@users.noreply.github.com>
2018-07-08 22:56:22 -0400

    Rgrzhang

--- Project_Draft/Supplementary_material/R/time_to_advantage_plot.R
+++ Project_Draft/Supplementary_material/R/time_to_advantage_plot.R
@@ -1,5 +1,5 @@
 library(ggplot2)
-ggplot(data=Advantage_time,mapping=aes(x=p,y=Time_to_advantage))+
+ggplot(data=Time_to_advantage,mapping=aes(x=p,y=Time))+
   geom_line()+
   geom_point()+
   labs(y="Years")
